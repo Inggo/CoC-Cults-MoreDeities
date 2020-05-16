a675620fb82ef8dc1df5849c0e6ad7a4d5578bd0
Inggo <inggo.espinosa@gmail.com>
2020-05-16 09:43:10 +0800

    Convert to v1.1 mod

---
+++ Source/MoreDeities.cs
@@ -0,0 +1,20 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace Inggo.MoreDeities
+{
+    public class MoreDeities : Mod
+    {
+        Settings settings;
+        public MoreDeities(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<Settings>();
+        }
+    }
+
+    public class Settings : ModSettings
+    {
+
+    }
+}
